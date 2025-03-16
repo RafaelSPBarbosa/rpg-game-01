d266b0af6ad56fa8420ed216f0a4cd9994ad759c
Rafael Barbosa <rafaspb11@gmail.com>
2025-03-16 11:03:57 -0300

    Added tooltip to NPC interaction

--- Scripts/NPCs/villager.gd
+++ Scripts/NPCs/villager.gd
@@ -1,27 +1,52 @@
-extends Node3D
+extends Node
 class_name Villager
 
 @onready var is_player_in_range = false
 @onready var is_talking_to_player = false
 
-@onready var player_talk_position = $PlayerTalkPosition
-@onready var camera_3d = $Camera3D
+@onready var player_talk_position = $Body/PlayerTalkPosition
+@onready var camera_3d = $Body/Camera3D
+
+@onready var interact_tooltip = $InteractTooltip
+@onready var interact_tooltip_initial_position := Vector3.ZERO
 
 @export var next_villager: Node3D = null
+@export var start_enabled: bool = true
 
 @export var lines : Array[String] = []
-var cur_line := 0
+@onready var cur_line := 0
+
+func _ready():
+	
+	interact_tooltip_initial_position = interact_tooltip.global_position
+	interact_tooltip.global_position.y -= 0.5
+	interact_tooltip.modulate.a = 0.0
+	
+	if start_enabled == false:
+		set_process(false)
+		set_physics_process(false)
+		set_process_input(false)
 
 func _on_area_3d_area_entered(area):
-	is_player_in_range = true
+	if area.is_in_group("Player"):
+		is_player_in_range = true
+		var tween = get_tree().create_tween()
+		tween.tween_property(interact_tooltip, "modulate:a", 1.0, .25).set_ease(Tween.EASE_OUT)
+		tween.tween_property(interact_tooltip, "global_position:y", interact_tooltip_initial_position.y, 0.25).set_ease(Tween.EASE_OUT)
+		
 
 func _on_area_3d_area_exited(area):
-	is_player_in_range = false
+	if area.is_in_group("Player"):
+		is_player_in_range = false
+		var tween = get_tree().create_tween()
+		tween.tween_property(interact_tooltip, "modulate:a", 0.0, .25).set_ease(Tween.EASE_OUT)
+		tween.tween_property(interact_tooltip, "global_position:y", interact_tooltip_initial_position.y - 0.5, 0.25).set_ease(Tween.EASE_OUT)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		if is_player_in_range == true:
 			if is_talking_to_player == false:
+				print("Player started talking to villager " + get_parent().get_parent().name)
 				is_talking_to_player = true
 				Player.instance.is_busy = true
 				Player.instance.body.global_position = player_talk_position.global_position
@@ -30,7 +55,6 @@ func _input(event: InputEvent) -> void:
 				camera_3d.current = true
 				cur_line = 0
 				say_line(lines[cur_line])
-				print("Player started talking to villager " + get_parent().get_parent().name)
 				return
 				
 		if is_talking_to_player == true:
@@ -43,8 +67,10 @@ func _input(event: InputEvent) -> void:
 				camera_3d.current = false
 				print("Player stopped talking to villager " + get_parent().get_parent().name)
 				if next_villager != null:
-					next_villager.visible = true
-					reparent(null)
+					next_villager.set_process(true)
+					next_villager.set_physics_process(true)
+					next_villager.set_process_input(true)
+					queue_free()
 					print("Replacing " + get_parent().get_parent().name + " with " + next_villager.name)
 			return
 
